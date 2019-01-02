ade130cf5244e81059ab8c9424704b0aaecaaa58
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2019-01-02 21:02:17 +0200

    Update compare.js

--- js/compare.js
+++ js/compare.js
@@ -6,8 +6,8 @@ function comparePassword(originalPass) {
   var attempts = 0;
   return function(userPass) {
     attempts++;
-  	if (attempts > 5) {
-    	alert('Много попыток');
+    if (attempts > 5) {
+      alert('Много попыток');
       return;
     }
     return originalPass === userPass;
